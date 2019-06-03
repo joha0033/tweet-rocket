48e67ffc29e71b2e559662c7abd0e73491820646
Austin <joha0033@gmail.com>
2019-06-03 14:02:41 -0600

    added twitter_id

--- db/migrations/production/20190529142003_users.js
+++ db/migrations/production/20190529142003_users.js
@@ -3,6 +3,7 @@ exports.up = (knex, Promise) => {
   return knex.schema.createTable('users', (table) => {
     table.increments().unsigned().primary();
     table.string('username').unique().notNullable();
+    table.string('twitter_id').unique().notNullable();
     table.string('displayName').unique().notNullable();
     table.string('provider').notNullable();
     table.boolean('admin').notNullable().defaultTo(false);
