079cf92d68a4379ca2f99109333f8f8e28ca4457
Austin <joha0033@gmail.com>
2019-06-03 22:50:42 -0600

    test config

--- knexfile.js
+++ knexfile.js
@@ -25,22 +25,15 @@ module.exports = {
     },
     useNullAsDefault: true
   },
-
   production: {
     client: 'pg',
+    // The next line is where the application will read that environment variable to connect to the database
     connection: process.env.DATABASE_URL,
-    // connection: {
-    //   user: process.env.DB_USER,
-    //   password: process.env.DB_PASS,
-    //   database: process.env.DB_NAME,
-    //   host: process.env.DATABASE_URL
-    // },
-    // migrations: {
-    //   directory: './db/migrations/production'
-    // },
-    // seeds: {
-    //   directory: './db/seeds/production'
-    // },
-    // useNullAsDefault: true
-  }
+    migrations: {
+      directory: __dirname + '/db/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/production',
+    },
+  },
 }
